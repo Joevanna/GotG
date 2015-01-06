847906f95212e3014f1d716c0608249a172b34a8
Giovanna Ruiu <giovanna.ruiu@analogfolk.com>
2015-01-06 20:37:23 +0000

    Creates json file for site

--- js/main.js
+++ js/main.js
@@ -1,49 +1,26 @@
-var characterData = {
-	handlerData:function(resJSON){
- 
-        var templateSource   = $("#characters-template").html(),
-
-            template = Handlebars.compile(templateSource),
-
-            charactersHTML = template(resJSON);
-
-       $('.container').html(studentHTML);
-         
-    },
-    loadCharactersData : function(){
-
-        $.ajax({
-            url:"http://gateway.marvel.com/v1/public/series/16410/characters",
-            method:'get',
-            success:this.handlerData
-        })
-    }
-}
-
-
 $(document).ready(function() {
-	$.ajax({
-		url: 'http://gateway.marvel.com/v1/public/series/16410',
-		type: 'GET',
-		dataType: 'json',
-		data: {
-			apikey: '1c917464a5be704887754ef19ffff579'
-		},
-		success: function(data) {
-			console.log(JSON.parse(data));
-		}
-	});
+	var url = "js/guardians.json";
+
+
 
 	setTimeout(function() {
 		$("img").addClass("active");
         $(".nav ul").addClass("visible");
 	}, 500);
 	
-	$('.nav li').click(function() {
-		$(this).toggleClass('selected');
+	// $('.nav li').click(function() {
+	// 	// $(this).toggleClass('selected');
 
-		// $('.wrapper').fadeOut('slow');
-		// $('.container').html(characterData.loadCharactersData());
-	});
+	// 	$('.wrapper').fadeOut('slow');
+	// 	// $('.container').html(characterData.loadCharactersData());
+
+	// 	if ($('li').hasClass('comic')) {
+	// 		var source = $('#comics-template').html(),
+	// 			template = Handlebars.compile(source);
+
+	// 		$('body').append(template(data));
+	// 	}
+	// });
 
 });
+
